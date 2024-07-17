d382c6f177ad3932a83e491f7d589f45a6ce1811
Dilip Shrestha <119152045+DilipSth@users.noreply.github.com>
2024-07-17 15:57:55 +0545

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import MobileNavbar from './components/Navbars/MobileNavbar'
+import Navbar from './components/Navbars/Navbar'
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <MobileNavbar />
+    </main>
+  )
+}
+
+export default App
